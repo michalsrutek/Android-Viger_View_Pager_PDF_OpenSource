849691552eec4903c86ede9a9b0ac247046983c3
Michal Srutek <michal.srutek@outlook.com>
2019-03-19 12:45:36 +0100

    added baseUrl, removed redundant imports

--- PDFVigerLib/src/main/java/com/necistudio/vigerpdf/network/RestClient.java
+++ PDFVigerLib/src/main/java/com/necistudio/vigerpdf/network/RestClient.java
@@ -1,33 +1,18 @@
 package com.necistudio.vigerpdf.network;
 
 
-import android.content.Context;
-import android.util.Log;
-
-
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
-import retrofit2.http.Part;
-import retrofit2.http.PartMap;
 import retrofit2.http.Streaming;
 import retrofit2.http.Url;
 
@@ -54,6 +39,7 @@ public class RestClient  {
             Retrofit client = new Retrofit.Builder()
                     .client(okclient)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("http://necistudio.com")
                     .build();
             ApiInterface = client.create(ApiInterface.class);
 
